3ab76e8a29c697b2092e81087ffbe3e06581f380
ohxo <109805956+ohxo@users.noreply.github.com>
2025-04-02 01:45:15 +0200

    add full inventory warning
    If the player has a full inventory, the action will be cancelled.

---
+++ src/main/java/me/nukleo/chaosgems/commands/ShopGemmeCommand.java
@@ -0,0 +1,72 @@
+package me.nukleo.chaosgems.commands;
+
+import me.nukleo.chaosgems.ChaosGems;
+import me.nukleo.chaosgems.utils.metodiUtili;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+public class ShopGemmeCommand implements CommandExecutor {
+    private File messages, playerData;
+    private ChaosGems plugin;
+    public ShopGemmeCommand(ChaosGems plugin, File playerData, File messages){
+        this.plugin=plugin;
+        this.playerData=playerData;
+        this.messages=messages;
+    }
+    
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if(command.getName().equalsIgnoreCase("shopgemme")){
+            FileConfiguration playerConfig = YamlConfiguration.loadConfiguration(playerData);
+            FileConfiguration messagesConfig = YamlConfiguration.loadConfiguration(messages);
+            
+            if(sender instanceof Player){
+                Player player = (Player) sender;
+                String message = messagesConfig.getString("errori.only-console");
+                player.sendMessage(metodiUtili.colora(message));
+            } else {
+                if(args.length != 2){
+                    String message = messagesConfig.getString("errori.shopgemme.missing-args");
+                    System.out.println(metodiUtili.colora(message));
+                } else {
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (target == null) {
+                        System.out.println(metodiUtili.colora("Il giocatore specificato non è online."));
+                        return true;
+                    }
+                    
+                    String shop = args[1];
+                    String uuid = target.getUniqueId().toString();
+                    int currGems = playerConfig.getInt("players." + uuid + ".gems", 0);
+                    int costo = this.plugin.getConfig().getInt("shop." + shop + ".costo");
+                    
+                    if(currGems >= costo){
+                        if (target.getInventory().firstEmpty() == -1) {
+                            String fullInventoryMessage = messagesConfig.getString("errori.shopgemme.full-inventory", "Inventario pieno");
+                            target.sendMessage(metodiUtili.colora(fullInventoryMessage));
+                            return true;
+                        }
+                        
+                        String execute = this.plugin.getConfig().getString("shop." + shop + ".command");
+                        String nome = target.getName();
+                        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "removegemme " + nome + " " + costo);
+                        execute = execute.replace("%player%", nome);
+                        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), execute);
+                    } else {
+                        String message = messagesConfig.getString("errori.shopgemme.not-enough");
+                        target.sendMessage(metodiUtili.colora(message));
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
